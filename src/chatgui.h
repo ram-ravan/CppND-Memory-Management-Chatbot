ee13b990694d5d5d6b55056bb44feb6d4a54edbf
ram-ravan <ram-baskar@web.de>
2022-03-25 20:22:35 +0100

    Task 1 - in progress

--- src/chatgui.h
+++ src/chatgui.h
@@ -28,7 +28,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
+    std::unique_ptr<ChatLogic> *GetChatLogicHandle() { return std::move(_chatLogic); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
